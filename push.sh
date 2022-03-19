56f54ab2adb5d88024b3a89e92c08a4e8d09be9e
zoorat <78788887+z00rat@users.noreply.github.com>
2022-03-19 05:40:15 +0000

    adding 'styling.lib.sh' features

--- push.sh
+++ push.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-echo " -> status of all branches"
+source './styling.lib.sh'
+
+LOG "Status of all branches"
 git --no-pager branch -v -v -a
 
-echo " -> git push --all origin"
+NEWLINE
+LOG "Pushing commites to origin"
+COMMAND "git push --all origin"
 git push --all origin
 
-echo " -> git push --tags"
+NEWLINE
+LOG "Pushing tags to origin"
+COMMAND "git push --tags"
 git push --tags
+
+NEWLINE
